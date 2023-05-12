ff1801c2f8a640a027f7c1009f56a3f4f52fad7c
Chartok <m.bhimjee@outlook.com>
2023-05-11 21:29:29 -0500

    added code: server.js; added directory for routing

--- Server.js
+++ Server.js
@@ -15,15 +15,19 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Setting up bare-bone conneection to database and session store; will be updated later as we add models and routes
-const sess = {
+
+const sess = { // This is the configuration object for the session middleware
     secret: '',
-    cookie: {},
+    cookie: {/* Add options here */},
     resave: '',
     saveUninitialized: '',
     store: new SequelizeStore({
         db: sequelize
     })
 };
+
+// Set up session middleware
+app.use(session(sess));
 
 // Set up Handlebars.js engine with custom helpers
 
@@ -34,3 +38,11 @@ const hbs = expressHandlebars.create({});
 
 
 // Inform Express.js on which template engine to use
+
+
+
+
+// sequelize.sync() operation will create and run the tables in the database based on the models defined. If the tables already exist, it will not re-create them unless you pass in { force: true } as an argument, which will force the sync operation to drop the table(s) before re-creating them.
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on ${PORT}. Visit http://localhost:${PORT} in your browser.'));
+});
