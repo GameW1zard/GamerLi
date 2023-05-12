72de63bfe764af0773b45dd0471e3508705d683e
Chartok <m.bhimjee@outlook.com>
2023-05-11 20:15:57 -0500

    updated packages to correct ones and imported in server.js

--- Server.js
+++ Server.js
@@ -1,14 +1,28 @@
 const path = require('path');
+
 const express = require('express');
+
 const sequelize = require('./config/connection');
+
 const expressHandlebars = require('express-handlebars');
+
+const session = require('express-session');
+
 
 const app = express();
 
+
+
 const PORT = process.env.PORT || 3001;
+
+
 
 // Set up Handlebars.js engine with custom helpers
 
+
+
 const hbs = expressHandlebars.create({});
-t 
+
+
+
 // Inform Express.js on which template engine to use
