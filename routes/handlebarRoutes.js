f239694efbf7190a1e2f66c521974f79a21eca27
GameW1zard <rivenloya@gmail.com>
2023-05-18 19:11:56 -0500

    routeing fixes

--- routes/handlebarRoutes.js
+++ routes/handlebarRoutes.js
@@ -1,4 +1,6 @@
 // Data for five people
+const router = require('express').Router();
+
 const data = {
     people: [
         {
@@ -50,36 +52,40 @@ const data = {
 };
 
 // Route to /homepage
-app.get('/homePage', (req, res) => {
+router.get('/homePage', (req, res) => {
     res.render('homePage');
 });
 
 // Route to /login
-app.get('/logIn', (req, res) => {
+router.get('/logIn', (req, res) => {
     res.render('logIn');
 });
 
 // Route to /aboutme
-app.get('/aboutMe', (req, res) => {
+router.get('/aboutMe', (req, res) => {
     res.render('aboutMe', data);
 });
 
 // Route to /contact
-app.get('/contact', (req, res) => {
+router.get('/contact', (req, res) => {
     res.render('contact', data);
 });
 
 // Route to /services
-app.get('/services', (req, res) => {
+router.get('/services', (req, res) => {
     res.render('services');
 });
 
 // Route to /register
-app.get('/register', (req, res) => {
+router.get('/register', (req, res) => {
     res.render('register');
 });
 
 // Route to /mylibrary
-app.get('/mylibrary', (req, res) => {
+router.get('/mylibrary', (req, res) => {
     res.render('mylibrary');
 });
+
+
+
+module.exports = router;
