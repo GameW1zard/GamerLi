fdb50cd368d787244f0146d651949b778715d528
Chartok <m.bhimjee@outlook.com>
2023-05-20 07:11:47 -0500

    reverting to previous commit

--- Server.js
+++ Server.js
@@ -2,8 +2,9 @@ const path = require('path');
 const helpers = require('./utils/helper');
 const express = require('express');
 const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars');
+const expressHandlebars = require('express-handlebars');
 const routes = require('./controllers');
+const hbs = expressHandlebars.create({ helpers });
 const session = require('express-session');
 
 // Initializes sequelize 'store' with express-session for connecting and baking cookies
@@ -35,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // Set Handlebars as the view engine
-app.engine('handlebars', exphbs({ helpers }));
+app.engine('handlebars', expressHandlebars(hbs));
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
